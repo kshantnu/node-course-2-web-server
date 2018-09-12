bae472650985a6836b0c489099ef21c2f8e8ee4e
Kumar Shantnu <kumarshantnu@Kumars-MacBook-Air.local>
2018-09-12 09:52:13 +0530

    mongodb fetch documents

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,24 @@
+const {MongoClient, ObjectId} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to mongodb server');
+    }
+    console.log('mongo db connection successful');
+    const db = client.db('TodoApp');
+    // db     .collection('Todos')     .insertOne({         text: 'my name is
+    // shivi',         wish: 'go to prague'     }, (err, result) => {         if
+    // (err) {             return console.log('something went wrong');         }
+    // console.log('record inserted',JSON.stringify(result.ops));     })
+
+    db
+        .collection('Todos')
+        .find({_id: new ObjectId("5b98001f9d8276da05dae8ba")})
+        .toArray()
+        .then((docs) => {
+            console.log(JSON.stringify(docs,undefined,2));
+        })
+        .catch((errorMessage) => {
+            console.log(`something went wrong ${errorMessage}`);
+        });
+    client.close();
+});
